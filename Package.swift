c4da5af271d30a513f0d36d577cb8315c4d45a52
Blair McArthur <blair.mcarthur@icloud.com>
2022-07-04 13:07:29 -0400

    Spm support (#1)
    Co-authored-by: Rob Nadin <robnadin@gmail.com>

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SocketRocket",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "SocketRocket",
+            targets: ["SocketRocket"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SocketRocket",
+            path: "SocketRocket",
+            publicHeadersPath: "PublicHeaders",
+            cSettings: [
+                .headerSearchPath("Internal/**"),
+            ]
+        ),
+    ]
+)
